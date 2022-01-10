260f8ffd4450a43bba9453d2a542797bbe68d797
Jhonatan Carvalho <jhonatancarvalhotorres12@gmail.com>
2022-01-10 19:57:33 -0300

    fix

---
+++ view/inputMaterial.php
@@ -0,0 +1,34 @@
+<?php include '../template/header.html';
+include '../template/menu.html'; ?>
+
+<div class="container">
+    <div class="row">
+        <h1 class="center-align">Histórico de entradas</h1>
+
+        <table class="centered responsive-table striped">
+            <thead>
+                <tr>
+                    <th>Requisitante</th>
+                    <th>Setor</th>
+                    <th>Imagem</th>
+                    <th>Material</th>
+                    <th>Quantidade</th>
+                    <th>Data</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>Alvin</td>
+                    <td>Biblioteca</td>
+                    <td><img class="materialboxed z-depth-1" src="https://a-static.mlcdn.com.br/1500x1500/tesoura-escolar-plus-13cm-ponta-arredondada-cores-sortidas-leonora/castronaves/11613/ab93a700958bf19206458c4884c49939.jpg"></td>
+                    <td>Caderno</td>
+                    <td>45</td>
+                    <td>12/03/2021</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+<?php include '../template/footer.html'; ?>
